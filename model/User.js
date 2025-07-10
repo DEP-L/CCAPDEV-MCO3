73ad092f8bae8c8a59fd4933722809488006cd9b
Danel <danel_limpiada@dlsu.edu.ph>
2025-07-10 20:53:01 +0800

    added model (user.js)

---
+++ model/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    accountType: { type: String, enum: ['student', 'technician'], required: true },
+    studentID: { type: Number, default: 0 },
+    techID: { type: Number, default: 0 },
+    displayName: { type: String, default: "" },
+    description: { type: String, default: "" },
+    image: { type: String, default: "" }
+});
+
+module.exports = mongoose.model('User', userSchema);
