ba9f9953663a6d2c4b07fb6ec1e6e0433b6094c2
Danel <danel_limpiada@dlsu.edu.ph>
2025-07-10 23:54:24 +0800

    added basic get routing (as well as temp post routes)

--- app.js
+++ app.js
@@ -18,15 +18,61 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// placeholder route
+// get routes
 app.get('/', (req, res) => {
-    res.render('');
+    res.render('partials/index', { title: 'Welcome' });
+});
+
+app.get('/login', (req, res) => {
+    res.render('partials/login', { title: 'Login' });
+});
+
+app.get('/register', (req, res) => {
+    res.render('partials/register', { title: 'Register' });
+});
+
+app.get('/dashboard', (req, res) => {
+    // add later: fetch labs and user info
+    res.render('partials/dash', { title: 'Dashboard' });
+});
+
+app.get('/labs', (req, res) => {
+    // add later: fetch lab list
+    res.render('partials/labs', { title: 'Manage Labs' });
+});
+
+app.get('/profile', (req, res) => {
+    // add later: fetch user profile and user reservations
+    res.render('partials/profile', {
+        title: 'Profile',
+        displayName: 'John Doe',
+        description: 'Sample user',
+        image: '/img/default.png',
+        reservation: []
+        // temp values for now
+    });
+});
+
+// temp post routes
+app.post('/create-lab', (req, res) => {
+    // add later: create lab in DB
+    res.send('Lab created');
+});
+
+app.post('/reserve-slot', (req, res) => {
+    // add later: reserve slot in DB
+    res.send('Slot reserved');
+});
+
+app.post('/edit-profile', (req, res) => {
+    // add later: update user profile
+    res.send('Profile updated');
 });
 
 app.listen(port, () => {
-    console.log('Server running at http://localhost:${port}');
+    console.log(`Server running at http://localhost:${port}`);
 });
