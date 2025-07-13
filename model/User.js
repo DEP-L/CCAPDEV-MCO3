9c1370a130792d8be8097ea822ac1c1b761f174a
Aaron <aaron_key@dlsu.edu.ph>
2025-07-14 07:24:52 +0800

    added static methods for generating student and tech ID's

--- model/User.js
+++ model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Reservation = require('./Reservation');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
@@ -8,7 +9,42 @@ const userSchema = new mongoose.Schema({
     techID: { type: Number, default: 0 },
     displayName: { type: String, default: "" },
     description: { type: String, default: "" },
-    image: { type: String, default: "" }
+    image: { type: String, default: "" },
+    reservations: { type: Array, default: [] }
 });
+
+// static method for generating student ID
+userSchema.statics.generateStudentID = async function() {
+    try {
+        const lastStudent = await this.findOne({ accountType: 'student' })
+                                      .sort({ studentID: -1 })
+                                      .lean();
+        if(lastStudent && lastStudent.studentID) {
+            return lastStudent.studentID + 1;
+        } else {
+            return 1001; // starting ID for students
+        }
+    } catch (err) {
+        console.error(err);
+        throw new Error('Failed to generate student ID.');
+    }
+}
+
+// static method for generating tech ID
+userSchema.statics.generateTechID = async function() {
+    try {
+        const lastTech = await this.findOne({ accountType: 'tech' })
+                                   .sort({ techID: -1 })
+                                   .lean();
+        if(lastTech && lastTech.techID) {
+            return lastTech.techID + 1;
+        } else {
+            return 2001; // starting ID for techs
+        }
+    } catch (err) {
+        console.error(err);
+        throw new Error('Failed to generate tech ID.')
+    }
+}
 
 module.exports = mongoose.model('User', userSchema);
