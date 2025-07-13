b43eb1895fabc199bc50ed5eb1792a113bfa6ff2
Aaron <aaron_key@dlsu.edu.ph>
2025-07-13 12:25:40 +0800

    updated Lab and Reservation models

--- model/Lab.js
+++ model/Lab.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const labSchema = new mongoose.Schema({
-    labID: { type: Number, default: 0 },
-    slots: { type: Array, default: [] }
+    labID: { type: Number, unique: true, default: 0 },
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    seatCount: { type: Number, default: 20 }
 });
 
 module.exports = mongoose.model('Lab', labSchema);
--- model/Reservation.js
+++ model/Reservation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const reservationSchema = new mongoose.Schema({
     reservationID: { type: Number, default: 0 },
-    labID: { type: Number, required: true },
+    labID: { type: Number, required: true, unique: true },
     studentID: { type: Number, required: true },
     reserveDate: { type: Date, required: true },
     requestDate: { type: Date, default: Date.now },
