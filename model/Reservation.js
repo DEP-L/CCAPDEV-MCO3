75a3c9450d619e415920b4758ebd8de2c735bb91
Aaron <aaron_key@dlsu.edu.ph>
2025-07-14 09:47:24 +0800

    added static method to add reservation ID onto reservation model

--- model/Reservation.js
+++ model/Reservation.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 
 const reservationSchema = new mongoose.Schema({
-    reservationID: { type: Number, default: 0 },
-    labID: { type: Number, required: true, unique: true },
+    reservationID: { type: Number, default: 0, unique: true },
+    labID: { type: Number, required: true },
     studentID: { type: Number, required: true },
     reserveDate: { type: Date, required: true },
     requestDate: { type: Date, default: Date.now },
     seatNumber: { type: Number, require: true},
     timeList: { type: Array, require: true }
+    // isAnon: { type: Boolean, default: true }
 });
+
+// static method for generating reservation ID
+reservationSchema.statics.generateReservationID = async function() {
+    try {
+        const lastReservation = await this.find().lean();
+        if(lastReservation && lastReservation.reservationID) {
+            return lastReservation.reservationID + 1;
+        } else {
+            return 3001; // starting ID for reservations
+        }
+    } catch (err) {
+        console.error(err);
+        throw new Error('Failed to generate reservation ID.');
+    }
+}
 
 module.exports = mongoose.model('Reservation', reservationSchema);
